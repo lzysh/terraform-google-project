df00cb7764662c8eba7e0056574120c84a8613b4
Brett Curtis <brett@lzy.sh>
2020-09-15 23:37:34 -0400

    Run controls for successful CIS benchmarks Fixes #3

--- test/integration/default-project/controls/cis_benchmark.rb
+++ test/integration/default-project/controls/cis_benchmark.rb
@@ -1,4 +1,7 @@
 require_controls 'inspec-gcp-cis-benchmark' do
+  control 'cis-gcp-1.1-iam'
+  control 'cis-gcp-1.5-iam'
+  control 'cis-gcp-1.6-iam'
   control 'cis-gcp-2.2-logging'
   control 'cis-gcp-3.1-networking'
   control 'cis-gcp-4.4-vms'
