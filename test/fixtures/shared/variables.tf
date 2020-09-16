f785627227f28b7ab6093c90cad71634bde2cb24
Brett Curtis <brett@lzy.sh>
2020-09-15 22:45:25 -0400

    Ensure sinks are configured for all log entries Fixes #1

--- test/fixtures/shared/variables.tf
+++ test/fixtures/shared/variables.tf
@@ -16,3 +16,13 @@ variable "folder_id" {
 variable "billing_id" {
   type = string
 }
+
+variable "cis_gcp_2_2_logging_bucket" {
+  type    = string
+  default = "cis-gcp-2-2-logging-sink"
+}
+
+variable "cis_gcp_2_2_logging_project" {
+  type    = string
+  default = "ops-tools-prod"
+}
